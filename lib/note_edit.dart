34a1af936626c16dcd8e8ba6fc2d943c44d6a50e
Roosa Pasanen <Roosa Pasanen>
2024-05-20 14:03:22 +0300

    note_edit.dart: Add loading data and back button

--- lib/note_edit.dart
+++ lib/note_edit.dart
@@ -4,15 +4,29 @@ import 'Note.dart';
 import 'package:flutter/services.dart';
 
 class NoteEditing extends StatefulWidget {
-  const NoteEditing({super.key});
+  final String title;
+  final String content;
+  const NoteEditing({super.key, required this.title, required this.content});
 
   @override
-  State<NoteEditing> createState() => _NoteEditingState();
+  State<NoteEditing> createState() =>
+      _NoteEditingState(title: title, content: content);
 }
 
 class _NoteEditingState extends State<NoteEditing> {
+  final String title;
+  final String content;
   final TextEditingController titleController = TextEditingController();
   final TextEditingController contentController = TextEditingController();
+
+  _NoteEditingState({required this.title, required this.content});
+
+  @override
+  void initState() {
+    titleController.text = title;
+    contentController.text = content;
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -24,22 +38,33 @@ class _NoteEditingState extends State<NoteEditing> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          textDirection: TextDirection.ltr,
+          controller: titleController,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              filled: true,
+              fillColor: Colors.blueAccent,
+              hintText: "New title"),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+            tooltip: "Return",
+          )
+        ],
+      ),
       body: Center(
           child: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const SizedBox(height: 20),
-          TextField(
-            textDirection: TextDirection.ltr,
-            controller: titleController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                filled: true,
-                fillColor: Colors.blueAccent,
-                hintText: "New title"),
-          ),
           const SizedBox(height: 30),
           Expanded(
               child: Container(
