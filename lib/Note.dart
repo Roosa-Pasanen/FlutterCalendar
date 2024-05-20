d976484e2f9eba041e3fe5f1c4a39ca70ec5c45f
Roosa Pasanen <Roosa Pasanen>
2024-05-20 13:00:04 +0300

    Note.dart: Define Note class

--- lib/Note.dart
+++ lib/Note.dart
@@ -1,1 +1,22 @@
-class Note {}
+class Note {
+  final int id;
+  final String title;
+  final String content;
+
+  Note(this.id, this.title, this.content);
+
+  Note.fromJson(Map<String, dynamic> json)
+      : id = json['id'] as int,
+        title = json['title'] as String,
+        content = json['content'] as String;
+
+  Map<String, dynamic> toJson() =>
+      {'id': id, 'title': title, 'content': content};
+}
+
+/*
+final DateTime lastOpened;
+this.lastOpened
+lastOpened = json['last-opened'] as DateTime;
+'last-opened': lastOpened
+*/
