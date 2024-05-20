44707fef1bc7f42cba2881e93a9a33dd89de4bb7
Roosa Pasanen <Roosa Pasanen>
2024-05-20 13:21:21 +0300

    note_listview.dart: Add json parsing from local database

--- lib/note_listview.dart
+++ lib/note_listview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
 class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
@@ -8,8 +10,20 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  List items = [];
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/notes.json');
+    final data = await json.decode(response);
+    print(data);
+    setState(() {
+      items = data["notes"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+        onPressed: readJson, child: const Text("show content"));
   }
 }
