15512c1aa0df93f46033388338feb1a7eb5b59c5
Roosa Pasanen <Roosa Pasanen>
2024-05-20 14:04:03 +0300

    note_listview.dart: Add navigation to list items

--- lib/note_listview.dart
+++ lib/note_listview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import "note_edit.dart";
 
 class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
@@ -15,37 +16,63 @@ class _MyWidgetState extends State<MyWidget> {
   Future<void> readJson() async {
     final String response = await rootBundle.loadString('assets/notes.json');
     final data = await json.decode(response);
-    print(data);
     setState(() {
       _items = data["notes"];
     });
   }
 
   @override
+  void initState() {
+    readJson();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: readJson,
-          child: const Text("show content"),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("View notes"),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back),
+              tooltip: "Return",
+            )
+          ],
         ),
-        _items.isNotEmpty
-            ? Expanded(
-                child: ListView.builder(
-                  itemCount: _items.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      margin: const EdgeInsets.all(10),
-                      child: ListTile(
-                        title: Text(_items[index]["title"]),
-                        subtitle: Text(_items[index]["content"]),
-                      ),
-                    );
-                  },
-                ),
-              )
-            : Container()
-      ],
-    );
+        body: Column(
+          children: [
+            _items.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: _items.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                            margin: const EdgeInsets.all(10),
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => NoteEditing(
+                                        title: _items[index]["title"],
+                                        content: _items[index]["content"]),
+                                  ),
+                                );
+                              },
+                              child: ListTile(
+                                title: Text(_items[index]["title"]),
+                                subtitle: Text(_items[index]["content"]),
+                              ),
+                            ));
+                      },
+                    ),
+                  )
+                : Container()
+          ],
+        ));
   }
 }
